550044138fd7239f3982b7df711bfda0fb92e0c9
radiant_wizard <hei.taratra@gmail.com>
2025-04-08 22:15:30 +0300

    chore(DishMapper): created a method to transform an Ingredient class to a IngredietDto class
    Took 2 minutes

--- src/main/java/com/Radiant_wizard/GastroManagementApp/mapper/DishMapper.java
+++ src/main/java/com/Radiant_wizard/GastroManagementApp/mapper/DishMapper.java
@@ -1,13 +1,17 @@
 package com.Radiant_wizard.GastroManagementApp.mapper;
 
+import com.Radiant_wizard.GastroManagementApp.entity.DTO.dish.DishIngredient;
+import com.Radiant_wizard.GastroManagementApp.entity.DTO.ingredient.IngredientBasicProperty;
 import com.Radiant_wizard.GastroManagementApp.entity.model.Dish;
 import com.Radiant_wizard.GastroManagementApp.entity.model.Ingredient;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class DishMapper {
     public List<Dish> resultSetToDish(ResultSet resultSet, List<Ingredient> ingredients) throws SQLException, IllegalAccessException {
         List<Dish> dishes = new ArrayList<>();
@@ -21,6 +25,26 @@ public class DishMapper {
             ));
         }
         return dishes;
+    }
+
+    public com.Radiant_wizard.GastroManagementApp.entity.DTO.dish.Dish dishDbToDish(Dish dish) {
+        com.Radiant_wizard.GastroManagementApp.entity.DTO.dish.Dish dishResponse = new com.Radiant_wizard.GastroManagementApp.entity.DTO.dish.Dish();
+        dishResponse.setId(dish.getDishId());
+        dishResponse.setName(dish.getDishName());
+        dishResponse.setIngredients(dish.getIngredients().stream()
+                .map
+                        (ingredient ->
+                                new DishIngredient(
+                                        ingredient.getNeededQuantity(),
+                                        ingredient.getUnit(),
+                                        new IngredientBasicProperty(
+                                                ingredient.getIngredientId()
+                                                , ingredient.getIngredientName()
+                                        )
+                                )).toList());
+        dishResponse.setActualPrice(Double.valueOf(dish.getPrice()));
+        dishResponse.setAvailableQuantity(dish.getAvailableQuantity());
+        return dishResponse;
     }
 
 }
