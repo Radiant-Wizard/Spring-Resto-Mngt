a840ffa9fa454a2a8e148f6e0d79e78489ebf924
radiant_wizard <hei.taratra@gmail.com>
2025-04-19 11:26:27 +0300

    refactor: removed unused import
    Took 45 minutes

--- src/main/java/com/Radiant_wizard/GastroManagementApp/entity/DTO/dish/Dish.java
+++ src/main/java/com/Radiant_wizard/GastroManagementApp/entity/DTO/dish/Dish.java
@@ -1,5 +1,6 @@
 package com.Radiant_wizard.GastroManagementApp.entity.DTO.dish;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
